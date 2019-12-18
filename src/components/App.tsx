d835bc7187faeb0ad9e28dc18ae511b1e90b9122
Darth Coder <00darthcoder00@gmail.com>
2019-12-18 10:25:40 +0200

    refactor the App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Header } from './Header';
-
+import { Link } from 'react-router-dom';
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Header />
+                <h1>Welcome to the app!</h1>
+                <Link to='/tasks'>Go to the Tasks page</Link>
             </div>
         );
     }
