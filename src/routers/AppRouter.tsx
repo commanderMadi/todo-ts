bb49be49f93ae7044b46a2f500abf17bea22ee8b
Darth Coder <00darthcoder00@gmail.com>
2019-12-18 10:26:27 +0200

    render the header in all components

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { App } from '../components/App';
+import { Header } from '../components/Header';
 import Tasks from '../components/Tasks';
 import { Profile } from '../components/Profile';
 import { Login } from '../components/Login';
@@ -9,10 +10,11 @@ export const AppRouter = () => {
     return (
         <BrowserRouter>
             <div>
+                <Header />
                 <Switch>
                     <Route path='/' exact component={App} />
                     <Route path='/tasks' component={Tasks} />
-                    <Route path='/profile' component={Profile} />
+                    <Route path='/profile' exact component={Profile} />
                     <Route path='/login' exact component={Login} />
                 </Switch>
             </div>
