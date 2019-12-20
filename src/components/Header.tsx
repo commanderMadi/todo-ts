5e3408cc61309c80fb20098d76c645ed6a666cb7
Darth Coder <00darthcoder00@gmail.com>
2019-12-20 12:02:59 +0200

    refactor header to render data programatically

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { ReduxStoreState } from '../reducers';
+import { Auth, checkAuthentication } from '../actions';
+import { connect } from 'react-redux';
 
-export const Header = () => {
+interface HeaderProps {
+    isAuthenticated: Auth;
+    checkAuthentication: typeof checkAuthentication;
+}
+
+export const Header = (props: HeaderProps) => {
+    const onLogoutclick = () => {
+        props.checkAuthentication(props.isAuthenticated);
+    };
     return (
         <ul>
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/tasks'>Tasks</NavLink>
             <NavLink to='/profile'>Profile</NavLink>
-            <NavLink to='/login'>Login</NavLink>
+            {!props.isAuthenticated.auth && (
+                <NavLink to='/login'>Login</NavLink>
+            )}
+            {props.isAuthenticated.auth && (
+                <button onClick={onLogoutclick}>Logout</button>
+            )}
         </ul>
     );
 };
+
+const mapStateToProps = (state: ReduxStoreState) => {
+    return {
+        isAuthenticated: state.isAuthenticated
+    };
+};
+
+export default connect(mapStateToProps, { checkAuthentication })(Header);
