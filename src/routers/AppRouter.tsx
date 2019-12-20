e6d99e88b2950618f031ef9e212b6353f490613c
Darth Coder <00darthcoder00@gmail.com>
2019-12-20 12:03:47 +0200

    config protected and public routes

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { App } from '../components/App';
-import { Header } from '../components/Header';
+import Header from '../components/Header';
 import Tasks from '../components/Tasks';
 import { Profile } from '../components/Profile';
-import { Login } from '../components/Login';
+import Login from '../components/Login';
+import { ReduxStoreState } from '../reducers';
+import { Auth } from '../actions';
+import { connect } from 'react-redux';
+import PrivateRoute from '../routers/PrivateRoute';
+import PublicRoute from './PublicRoute';
 
-export const AppRouter = () => {
+interface AppRouterProps {
+    isAuthenticated: Auth;
+}
+
+export const AppRouter = (props: AppRouterProps) => {
     return (
         <BrowserRouter>
             <div>
                 <Header />
                 <Switch>
                     <Route path='/' exact component={App} />
-                    <Route path='/tasks' component={Tasks} />
-                    <Route path='/profile' exact component={Profile} />
-                    <Route path='/login' exact component={Login} />
+                    <PrivateRoute path='/tasks' component={Tasks} />
+                    <PrivateRoute path='/profile' component={Profile} />
+                    <PublicRoute path='/login' component={Login} />
                 </Switch>
             </div>
         </BrowserRouter>
     );
 };
+
+const mapStateToProps = (state: ReduxStoreState) => {
+    return {
+        isAuthenticated: state.isAuthenticated
+    };
+};
+
+export default connect(mapStateToProps)(AppRouter);
