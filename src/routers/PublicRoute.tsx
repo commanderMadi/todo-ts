9aad3805be5723140c8d9b9684441808f3c9e9e3
Darth Coder <00darthcoder00@gmail.com>
2019-12-20 12:04:11 +0200

    create PublicRoute component

---
+++ src/routers/PublicRoute.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { ReduxStoreState } from '../reducers';
+import { Auth } from '../actions';
+
+interface PublicRouteProps {
+    isAuthenticated: Auth;
+    exact?: boolean;
+    path: string;
+    component: React.ComponentType<any>;
+}
+
+export const PublicRoute = ({
+    isAuthenticated,
+    component: Component,
+    ...rest
+}: PublicRouteProps) => (
+    <Route
+        {...rest}
+        component={(props: PublicRouteProps) => {
+            console.log(props);
+            return isAuthenticated.auth ? (
+                <Redirect to='/' />
+            ) : (
+                <div>
+                    <Component {...props} />
+                </div>
+            );
+        }}
+    />
+);
+const mapStateToProps = (state: ReduxStoreState) => ({
+    isAuthenticated: state.isAuthenticated
+});
+
+export default connect(mapStateToProps)(PublicRoute);
