116dc58ae7ad1647186f2626ed871ad3931eca29
Darth Coder <00darthcoder00@gmail.com>
2019-12-22 12:27:59 +0200

    Updates

---
+++ precache-manifest.3b0d261e055a4c15b325ca86d0a85ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b508b1666e7e2631d4cbdada988749",
+    "url": "/todo-ts/index.html"
+  },
+  {
+    "revision": "47df6e92db333728c58f",
+    "url": "/todo-ts/static/js/2.f8c2dce9.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/todo-ts/static/js/2.f8c2dce9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1563b63582047e7bf453",
+    "url": "/todo-ts/static/js/main.c8219677.chunk.js"
+  },
+  {
+    "revision": "f3830bf28df3695b8c42",
+    "url": "/todo-ts/static/js/runtime-main.551826ff.js"
+  }
+]);
