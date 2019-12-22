bc3ae27d3e7efd19fd6503c82bca759f760cae5c
Ahmed Magdy <madimagdy@gmail.com>
2019-12-22 04:56:27 +0200

    rename todos to tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,67 +1,65 @@
 import React from 'react';
 import { ReduxStoreState } from '../reducers/';
 import { connect } from 'react-redux';
-import { Todo, Category, getCategories, deleteTodo } from '../actions';
-import AddTodoForm from '../components/AddTodoForm';
+import { Task, Category, getCategories, deleteTask } from '../actions';
+import AddTaskForm from '../components/AddTaskForm';
 
 interface TasksProps {
-    categories: Category[];
-    getCategories: Function;
-    deleteTodo: typeof deleteTodo;
+  categories: Category[];
+  getCategories: Function;
+  deleteTask: typeof deleteTask;
 }
 
 class Tasks extends React.Component<TasksProps> {
-    componentDidMount() {
-        this.props.getCategories();
-    }
+  componentDidMount() {
+    this.props.getCategories();
+  }
 
-    onBtnClick = (id: string) => {
-        this.props.deleteTodo(id);
-    };
+  onBtnClick = (id: string) => {
+    this.props.deleteTask(id);
+  };
 
-    renderCategories() {
+  renderCategories() {
+    return (
+      this.props.categories &&
+      this.props.categories.map((category: Category, index: number) => {
         return (
-            this.props.categories &&
-            this.props.categories.map((category: Category, index: number) => {
-                return (
-                    <div key={index}>
-                        <h2>{category.name}</h2>
+          <div key={index}>
+            <h2>{category.name}</h2>
 
-                        {category.todos.map((todo: Todo) => {
-                            return (
-                                <div key={todo.id}>
-                                    <h4>{todo.title}</h4>
-                                    <button
-                                        onClick={() => this.onBtnClick(todo.id)}
-                                    >
-                                        Remove Todo
-                                    </button>
-                                </div>
-                            );
-                        })}
-                    </div>
-                );
-            })
+            {category.tasks.map((task: Task) => {
+              return (
+                <div key={task.id}>
+                  <h4>{task.title}</h4>
+                  <button onClick={() => this.onBtnClick(task.id)}>
+                    Remove Task
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         );
-    }
-    render() {
-        return (
-            <div>
-                <h1>Tasks Component</h1>
-                <AddTodoForm categories={this.props.categories} />
-                {this.props.categories.length > 0 ? (
-                    this.renderCategories()
-                ) : (
-                    <p>No data</p>
-                )}
-            </div>
-        );
-    }
+      })
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h1>Tasks Component</h1>
+        <AddTaskForm categories={this.props.categories} />
+        {this.props.categories.length > 0 ? (
+          this.renderCategories()
+        ) : (
+          <p>No data</p>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: ReduxStoreState) => {
-    return {
-        categories: state.categories
-    };
+  return {
+    categories: state.categories
+  };
 };
-export default connect(mapStateToProps, { getCategories, deleteTodo })(Tasks);
+export default connect(mapStateToProps, { getCategories, deleteTask })(Tasks);
