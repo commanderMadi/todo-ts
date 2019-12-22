773f23374942bbefedd34e13ab9e6d2a647e557b
Ahmed Magdy <madimagdy@gmail.com>
2019-12-22 04:55:50 +0200

    move tasks page navigation to profile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome to the app!</h1>
-                <Link to='/tasks'>Go to the Tasks page</Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the app!</h1>
+      </div>
+    );
+  }
 }
 
 export { App };
