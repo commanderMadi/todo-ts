6657e03f6a9bd985dc66c46b0aef2402c6f6d0f6
Ahmed Magdy <madimagdy@gmail.com>
2019-12-23 04:44:45 +0200

    Add styling to login error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,80 +4,71 @@ import { ReduxStoreState } from '../reducers';
 import { checkAuthentication, Auth } from '../actions';
 
 interface LoginProps {
-    isAuthenticated: Auth;
-    checkAuthentication: typeof checkAuthentication;
+  isAuthenticated: Auth;
+  checkAuthentication: typeof checkAuthentication;
 }
 
 export class Login extends React.Component<LoginProps> {
-    state = { username: 'Admin', password: '54321', error: null };
+  state = { username: 'Admin', password: '54321', error: null };
 
-    componentDidMount() {
-        const loginData = JSON.stringify(this.state);
-        localStorage.setItem('credentials', loginData);
+  componentDidMount() {
+    const loginData = JSON.stringify(this.state);
+    localStorage.setItem('credentials', loginData);
+  }
+
+  checkCredentials(username: string, password: string) {
+    const data: any = localStorage.getItem('credentials');
+    const parsedData = JSON.parse(data);
+
+    if (username === parsedData.username && password === parsedData.password) {
+      this.props.checkAuthentication(this.props.isAuthenticated);
+    } else {
+      this.setState(() => {
+        return {
+          error: 'Wrong username or password.'
+        };
+      });
     }
+  }
 
-    checkCredentials(username: string, password: string) {
-        const data: any = localStorage.getItem('credentials');
-        const parsedData = JSON.parse(data);
+  onFormSubmit = (e: any) => {
+    e.preventDefault();
+    const [username, password] = e.target.elements;
+    this.checkCredentials(username.value, password.value);
+  };
 
-        if (
-            username === parsedData.username &&
-            password === parsedData.password
-        ) {
-            this.props.checkAuthentication(this.props.isAuthenticated);
-        } else {
-            this.setState(() => {
-                return {
-                    error: 'Wrong username or password.'
-                };
-            });
-        }
-    }
-
-    onFormSubmit = (e: any) => {
-        e.preventDefault();
-        const [username, password] = e.target.elements;
-        this.checkCredentials(username.value, password.value);
-    };
-
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onFormSubmit}>
-                    <div className='form-group'>
-                        <label htmlFor='user'>Username</label>
-                        <input
-                            type='text'
-                            className='form-control'
-                            id='usernameinput'
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='pass'>Password</label>
-                        <input
-                            type='password'
-                            className='form-control'
-                            id='passwordinput'
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <input
-                            value='Log In'
-                            type='submit'
-                            className='btn btn-primary'
-                        />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.error && (
+          <p className='col-md-12 text-danger mt-4'>{this.state.error}</p>
+        )}
+        <form onSubmit={this.onFormSubmit}>
+          <div className='form-group'>
+            <label htmlFor='user'>Username</label>
+            <input type='text' className='form-control' id='usernameinput' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='pass'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='passwordinput'
+            />
+          </div>
+          <div className='form-group'>
+            <input value='Log In' type='submit' className='btn btn-primary' />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: ReduxStoreState) => {
-    return {
-        isAuthenticated: state.isAuthenticated
-    };
+  return {
+    isAuthenticated: state.isAuthenticated
+  };
 };
 
 export default connect(mapStateToProps, { checkAuthentication })(Login);
