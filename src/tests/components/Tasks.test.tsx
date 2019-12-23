0de20d52ec0ae3d29b82373e2f1059cd47704912
Ahmed Magdy <madimagdy@gmail.com>
2019-12-23 04:45:11 +0200

    Add test for adding tasks

--- src/tests/components/Tasks.test.tsx
+++ src/tests/components/Tasks.test.tsx
@@ -5,53 +5,86 @@ import { Task } from '../../actions';
 import { ActionTypes } from '../../actions/actiontypes';
 import { initialData } from '../fixture/testdata';
 
-let newTask: Task, wrapper: ShallowWrapper<Tasks>;
+let newTask: Task;
 beforeEach(() => {
-    newTask = {
-        id: 'default-id-of-dummy-data',
-        title: 'Run for half an hour',
-        categoryTitle: 'Sports',
-        error: null
-    };
-    wrapper = shallow(
-        <Tasks
-            categories={initialData}
-            getCategories={() => ({
-                type: ActionTypes.getCategories
-            })}
-            deleteTask={id => ({
-                type: ActionTypes.deleteTask,
-                payload: { id }
-            })}
-            addTask={() => {
-                return {
-                    type: ActionTypes.addTask,
-                    payload: newTask
-                };
-            }}
-        />
-    );
+  newTask = {
+    id: 'default-id-of-dummy-data',
+    title: 'Run for half an hour',
+    categoryTitle: 'Sports',
+    error: null
+  };
 });
 
 test('Tasks component should load with all default data', () => {
-    expect(wrapper).toMatchSnapshot();
+  const wrapper: ShallowWrapper<Tasks> = shallow(
+    <Tasks
+      categories={initialData}
+      getCategories={() => ({
+        type: ActionTypes.getCategories
+      })}
+      deleteTask={id => ({
+        type: ActionTypes.deleteTask,
+        payload: { id }
+      })}
+      addTask={() => {
+        return {
+          type: ActionTypes.addTask,
+          payload: newTask
+        };
+      }}
+    />
+  );
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('A task should be deleted when delete function is triggered', () => {
-    const onDeleteSpy = jest.spyOn(Tasks.prototype, 'onDelete');
+  const onDeleteSpy = jest.spyOn(Tasks.prototype, 'onDelete');
+  const wrapper: ShallowWrapper<Tasks> = shallow(
+    <Tasks
+      categories={initialData}
+      getCategories={() => ({
+        type: ActionTypes.getCategories
+      })}
+      deleteTask={id => ({
+        type: ActionTypes.deleteTask,
+        payload: { id }
+      })}
+      addTask={() => {
+        return {
+          type: ActionTypes.addTask,
+          payload: newTask
+        };
+      }}
+    />
+  );
 
-    wrapper
-        .find('button.task_remove')
-        .at(1)
-        .simulate('click');
-    expect(onDeleteSpy).toHaveBeenLastCalledWith(initialData[1].tasks[0].id);
+  wrapper
+    .find('button.task_remove')
+    .at(1)
+    .simulate('click');
+  expect(onDeleteSpy).toHaveBeenLastCalledWith(initialData[1].tasks[0].id);
 });
 
-/* Still working on this test */
-
-// test('AddTaskForm should correctly adds a task to the Tasks page on submit', () => {
-//     const onSubmit = jest.spyOn(Tasks.prototype, 'onSubmit');
-//     wrapper.find('AddTaskForm').prop('onSubmit')!(newTask as any);
-//     const { title, id } = newTask;
-//     expect(onSubmit).toHaveBeenLastCalledWith({ title, id });
-// });
+test('AddTaskForm should correctly adds a task to the Tasks page on submit', () => {
+  const onSubmit = jest.spyOn(Tasks.prototype, 'onSubmit');
+  const wrapper: ShallowWrapper<Tasks> = shallow(
+    <Tasks
+      categories={initialData}
+      getCategories={() => ({
+        type: ActionTypes.getCategories
+      })}
+      deleteTask={id => ({
+        type: ActionTypes.deleteTask,
+        payload: { id }
+      })}
+      addTask={() => {
+        return {
+          type: ActionTypes.addTask,
+          payload: newTask
+        };
+      }}
+    />
+  );
+  wrapper.find('AddTaskForm').prop('onSubmit')!(newTask as any);
+  expect(onSubmit).toHaveBeenLastCalledWith(newTask);
+});
